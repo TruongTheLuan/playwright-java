b4a3950bb72a96e3c4eb2142867178b0cc5a9077
luantruong <truongluan100695@gmail.com>
2024-11-10 10:09:16 +0700

    [ID-8] Update Radio

--- src/test/java/testcases/elements/RadioTest.java
+++ src/test/java/testcases/elements/RadioTest.java
@@ -9,16 +9,19 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RadioTest extends MasterTest{
-    @ParameterizedTest
-    @ValueSource(strings = { "Apple", "Pear", "Orange" })
-    void verifyUserCanSelectOptionInDropdown(String radioValue) {
-        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/radio");
-        assertThat(page).hasTitle("Test With Me aka Tho Test");
+    void selectItemInRadio(String radioValue){
         String radioXpath = String.format("//input[@value='%s' and contains(concat(' ',normalize-space(@class),' '),' ant-radio-input ')]", radioValue);
         Locator radioLocator = page.locator(radioXpath);
-        Locator valueLocator = page.locator("//input[@value='Apple' and contains(concat(' ',normalize-space(@class),' '),' ant-radio-input ')]//following::div[contains(.,'Value:')][1]");
         radioLocator.check();
         assertTrue(radioLocator.isChecked());
+    }
+    @ParameterizedTest
+    @ValueSource(strings = { "Apple", "Pear", "Orange" })
+    void verifyUserCanSelectItemInRadio(String radioValue) {
+        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/elements/radio");
+        assertThat(page).hasTitle("Test With Me aka Tho Test");
+        Locator valueLocator = page.locator("//input[@value='Apple' and contains(concat(' ',normalize-space(@class),' '),' ant-radio-input ')]//following::div[contains(.,'Value:')][1]");
+        selectItemInRadio(radioValue);
         assertThat(valueLocator).hasText(String.format("Value: %s", radioValue));
     }
 }
