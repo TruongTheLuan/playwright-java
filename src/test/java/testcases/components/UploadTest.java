5698355d2b6db1e873f5f83d968255a09a10dd37
luantruong <truongluan100695@gmail.com>
2024-11-23 21:38:13 +0700

    [ID-22] Implement upload file

--- src/test/java/testcases/components/UploadTest.java
+++ src/test/java/testcases/components/UploadTest.java
@@ -5,36 +5,25 @@ import com.microsoft.playwright.Locator;
 import org.junit.jupiter.api.Test;
 import testcases.MasterTest;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class UploadTest extends MasterTest {
     @Test
-    void verifyUpload() throws InterruptedException {
-        String fileName = "test01.pdf";
+    void verifyUploadFile(){
         page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/components/upload");
         assertThat(page).hasTitle("Test With Me aka Tho Test");
-        String buttonUploadXpath = "//button[normalize-space(.//text())='Click to Upload']";
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL inputStream = classLoader.getResource("upload/my-upload-file.txt");
+        String buttonUploadXpath = "//span[contains(concat(' ',normalize-space(@class),' '),' ant-upload ')]//button[normalize-space(.//text())='Click to Upload']";
         Locator buttonUploadLocator = page.locator(buttonUploadXpath);
-        FileChooser fileChooser = page.waitForFileChooser(() -> buttonUploadLocator.click());
-        fileChooser.setFiles(Paths.get(String.format("/Users/geotech/Desktop/%s", fileName)));
-        String expectedElementXpath = String.format("//span[normalize-space(.//text())='test01.pdf']", fileName);
+        buttonUploadLocator.setInputFiles(Path.of(inputStream.getPath()));
+        String expectedElementXpath = "//span[normalize-space(.//text())='my-upload-file.txt']";
         Locator expectedElementLocator = page.locator(expectedElementXpath);
-        assertThat(expectedElementLocator).hasText(fileName);
-    }
-
-    @Test
-    void verifyRemoveFileAfterUpload() throws InterruptedException {
-        String fileName = "test01.pdf";
-        page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/components/upload");
-        assertThat(page).hasTitle("Test With Me aka Tho Test");
-        String buttonUploadXpath = "//button[normalize-space(.//text())='Click to Upload']";
-        Locator buttonUploadLocator = page.locator(buttonUploadXpath);
-        FileChooser fileChooser = page.waitForFileChooser(() -> buttonUploadLocator.click());
-        fileChooser.setFiles(Paths.get(String.format("/Users/geotech/Desktop/%s", fileName)));
-        String expectedElementXpath = String.format("//span[normalize-space(.//text())='test01.pdf']", fileName);
-        Locator expectedElementLocator = page.locator(expectedElementXpath);
-        assertThat(expectedElementLocator).hasText(fileName);
+        assertThat(expectedElementLocator).hasText("my-upload-file.txt");
     }
 }
