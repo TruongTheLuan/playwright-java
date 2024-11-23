5513582a6f91c48937b37f4b0bcaf9218e612a0a
luantruong <truongluan100695@gmail.com>
2024-11-23 20:56:17 +0700

    [ID-21] Implement tree select

--- src/test/java/testcases/components/TreeSelectTest.java
+++ src/test/java/testcases/components/TreeSelectTest.java
@@ -7,39 +7,40 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import testcases.MasterTest;
 
+import java.util.Locale;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class TreeSelectTest extends MasterTest {
-    /*@ParameterizedTest
-    @ValueSource(strings = { "Light", "Bamboo", "Cedar", "Pine", "Oak" })*/
-
     @ParameterizedTest
-    @CsvSource({
-            "Light,         Light",
-            "Light,         Bamboo",
-            "Light,         Cedar",
-            "Light,         Pine",
-            "Light,        Oak",
-            "Heavy,        Heavy",
-            "Heavy,        Mahogany",
-            "Heavy,        Walnut"
-    })
-    void verifyTreeSelect(String treeSwitcher, String input){
+    @ValueSource(strings = { "Light > Bamboo", "Heavy > Walnut", "Light > Pine", "Heavy" })
+    void verifyTreeSelect(){
+        String inputs = "Heavy > Walnut";
         page.navigate("https://test-with-me-app.vercel.app/learning/web-elements/components/tree-select");
         assertThat(page).hasTitle("Test With Me aka Tho Test");
         String inputXpath = "//div[contains(concat(' ',normalize-space(@class),' '),' ant-select-selector ')]";
         Locator inputLocator = page.locator(inputXpath);
         inputLocator.click();
 
-        String treeSwitcherXpath= String.format("(//span[normalize-space(text())='%s']//preceding::span[contains(concat(' ',normalize-space(@class),' '),' ant-select-tree-switcher ')])[last()]", treeSwitcher);
-        Locator treeSwitcherLocator = page.locator(treeSwitcherXpath);
-        treeSwitcherLocator.click();
-
-        String inputItemXpath = String.format("//span[normalize-space(text())='%s']", input);
-        Locator inputItemLocator = page.locator(inputItemXpath);
-        inputItemLocator.click();
+        String[] inputStrings = inputs.split(">");
+        for(int i=0; i < inputStrings.length; i++){
+            if(i < inputStrings.length - 1){
+                String arrowXpath= String.format("(//span[normalize-space(text())='%s']//preceding::span[contains(concat(' ',normalize-space(@class),' '),' ant-select-tree-switcher ')])[last()]", inputStrings[i].trim());
+                Locator arrowXLocator = page.locator(arrowXpath);
+                String classValue = arrowXLocator.getAttribute("class");
+                if(!classValue.contains("open")){
+                    arrowXLocator.click();
+                }
+            }
+            else{
+                String inputItemXpath = String.format("//span[contains(concat(' ',normalize-space(@class),' '),' ant-select-tree-title ')][normalize-space(text())='%s']", inputStrings[i].trim());
+                Locator inputItemLocator = page.locator(inputItemXpath);
+                inputItemLocator.click();
+            }
+        }
 
         String expectedElementXpath = "//div[contains(., 'Current value: ') and ./span[contains(concat(' ', normalize-space(@class),' '),' text-rose-500 ')]]";
-        assertThat(page.locator(expectedElementXpath)).hasText(String.format("Current value: %s", input.toLowerCase()));
+        assertThat(page.locator(expectedElementXpath)).hasText(String.format("Current value: %s", inputStrings[inputStrings.length - 1].trim().toLowerCase()));
+
     }
 }
